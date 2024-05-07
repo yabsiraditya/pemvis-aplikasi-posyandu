d1cb11206709c7d3ec3b1a745f34a2b3e3d7bba0
PREDATOR <PREDATOR@ARCHO>
2024-05-07 10:35:26 +0700

    aco commit update

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+/**
+ *
+ * @author PREDATOR
+ */
+public class koneksi {
+    private Connection koneksi;
+        public Connection connect(){
+            try{
+                Class.forName("com.mysql.jdbc.Driver"); 
+                System.out.println("berhasil connect");
+            }catch (ClassNotFoundException ex) {
+                    System.out.println("gagal koneksi"+ex);
+                }
+                String url = "jdbc:mysql://localhost:3306/posyandu";
+            try{
+                koneksi = DriverManager.getConnection(url,"root","");
+                System.out.println("berhasil mengkoneksi database");
+            }catch (SQLException ex) {
+                    System.out.println("gagal mengkoneksi database"+ex);
+                }
+                return koneksi;
+        }
+}
